cea2039e9110dfc20cf94f5dcc3da685e6bc7c72
OKsusueyes <inyoung1995@gmail.com>
2026-06-15 14:15:28 +0900

    Update server.js
    서버 다시 재업

--- server.js
+++ server.js
@@ -7,25 +7,23 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 클라이언트에게 화면을 보여주기 위해 public 폴더를 엽니다.
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('새로운 사용자가 연결되었습니다!');
 
-    // 🌟 1. 사용자가 접속하며 자신의 닉네임을 알려주면, 서버가 그 닉네임을 '기억'합니다.
+    // 사용자가 접속하며 자신의 닉네임을 알려주면 서버가 기억합니다.
     socket.on('user connect notification', (nickname) => {
-        socket.nickname = nickname; // 소켓(개인 연결)에 닉네임 저장
-        // 본인을 제외한 다른 모든 접속자들에게 새로운 사람의 입장을 알립니다.
+        socket.nickname = nickname; 
         socket.broadcast.emit('user joined', nickname);
     });
 
-    // 클라이언트로부터 채팅 데이터를 받았을 때
+    // 채팅 데이터를 받았을 때 다른 사람들에게 뿌려줍니다.
     socket.on('chat message', (data) => {
         socket.broadcast.emit('chat message', data);
     });
 
-    // 🌟 2. 사용자가 나갔을 때 (저장해둔 닉네임을 사용해 퇴장 알림 방송)
+    // 사용자가 나갔을 때 저장해둔 닉네임으로 퇴장 알림을 방송합니다.
     socket.on('disconnect', () => {
         if (socket.nickname) {
             socket.broadcast.emit('user left', socket.nickname);
