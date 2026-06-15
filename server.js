b7e10e3749008749e80ef4d2ce102e1d6af437e7
OKsusueyes <inyoung1995@gmail.com>
2026-06-15 13:38:26 +0900

    Update server.js
    접속 알리미 기능 추가 server.js 업데이트

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ const io = new Server(server);
 // 클라이언트(접속자)에게 화면을 보여주기 위해 public 폴더를 엽니다.
 app.use(express.static('public'));
 
-// 누군가 웹사이트에 접속했을 때 실행됩니다.
 io.on('connection', (socket) => {
     console.log('새로운 사용자가 비눗방울 공원에 입장했습니다!');
+
+    // 🌟 새로운 사용자가 입장해서 자신의 닉네임을 보냈을 때 실행됩니다.
+    socket.on('user connect notification', (nickname) => {
+        // 본인을 제외한 다른 모든 접속자들에게 새로운 사람의 닉네임을 전송합니다.
+        socket.broadcast.emit('user joined', nickname);
+    });
 
     // 클라이언트로부터 'chat message'라는 이름의 데이터를 받았을 때
     socket.on('chat message', (data) => {
